17b1f82bb314d74a8a9db7b6af6b5b27c0251589
2pandi <coder.2pandi@gmail.com>
2022-10-19 22:29:37 +0900

    :sparkles: feat: Fetch mock data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { useEffect, useState } from "react";
+import Ads from "./components/Ads";
+import BoardMain from "./components/BoardMain";
+
 function App() {
-  return <div className="App"></div>;
+  const [freeItems, setFreeItems] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3001/vote")
+      .then((res) => res.json())
+      .then((data) => setFreeItems(data))
+      .catch((e) => console.log(e));
+  });
+  return (
+    <div className="App">
+      <BoardMain items={freeItems} />
+      <Ads />
+    </div>
+  );
 }
 
 export default App;
