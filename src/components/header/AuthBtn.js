8e5a95eff3513ac9098e5ccf42b0ed6ae996bbf5
HyeonWooGa <yeonwoopark22@gmail.com>
2022-10-20 16:29:20 +0900

    ✨ feat: 헤더(탑바네비게이션) 컴포넌트 추가

---
+++ src/components/header/AuthBtn.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  gap: 30px;
+  margin-left: 100px;
+`;
+
+const LoginBtn = styled.button`
+  height: 40px;
+  width: 80px;
+  border: 1px solid blue;
+  border-radius: 20px;
+`;
+
+const JoinBtn = styled.button`
+  height: 40px;
+  width: 80px;
+  border: 1px solid blue;
+  border-radius: 20px;
+  background-color: blue;
+  color: white;
+`;
+
+function AuthBtn() {
+  return (
+    <Wrapper>
+      <LoginBtn>Login</LoginBtn>
+      <JoinBtn>Join</JoinBtn>
+    </Wrapper>
+  );
+}
+
+export default AuthBtn;
