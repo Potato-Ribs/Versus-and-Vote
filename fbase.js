7b03949dad0708168b8e4c03621ecc9d866be40e
2pandi <coder.2pandi@gmail.com>
2022-10-24 08:41:49 +0900

    :sparkles: feat: Set firebase

---
+++ fbase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MEASUREMENTID,
+};
+
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
