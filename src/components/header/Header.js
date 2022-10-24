cd1c224d118c6663cc00b9892d1ab723ade4384c
2pandi <coder.2pandi@gmail.com>
2022-10-25 03:07:55 +0900

    :sparkles: feat: Check Auth, Add user setting toggle

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 import AuthBtn from "./AuthBtn";
 import DarkModeBtn from "./DarkModeBtn";
 import Search from "./Search";
+import { useState } from "react";
+import { auth } from "../../fbase";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 
 const Container = styled.header`
   width: 100vw;
@@ -10,6 +13,31 @@ const Container = styled.header`
   align-items: center;
   justify-content: center;
   padding: 4vh 4vw;
+
+  .user-toggle {
+    position: relative;
+  }
+
+  .user-avatar {
+    width: 40px;
+    border-radius: 50%;
+    position: relative;
+  }
+  .user-setting-list {
+    width: 5rem;
+    background-color: orange;
+    position: absolute;
+    z-index: 1;
+  }
+  .setting-item {
+    height: 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    :hover {
+      background-color: gray;
+    }
+  }
 `;
 
 const Nav = styled.nav`
@@ -29,6 +57,24 @@ const Ul = styled.ul`
 const Li = styled.li``;
 
 function Header() {
+  const [userAvatar, setUserAvatar] = useState(null);
+  const [toggle, setToggle] = useState(false);
+
+  useState(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) setUserAvatar(user.photoURL);
+    });
+  }, []);
+
+  const onLogoutClick = () => {
+    signOut(auth);
+    document.location.href = "/";
+  };
+
+  const openToggle = () => {
+    setToggle(!toggle);
+  };
+
   return (
     <Container>
       <Nav>
@@ -44,8 +90,29 @@ function Header() {
         </Ul>
       </Nav>
       <Search />
-      <AuthBtn />
       <DarkModeBtn />
+      {userAvatar ? (
+        <>
+          <div className="user-toggle">
+            <img
+              className="user-avatar"
+              onClick={() => openToggle()}
+              src={userAvatar}
+              alt="user avatar"
+            />
+            {toggle && (
+              <div className="user-setting-list">
+                <div className="setting-item">내 프로필</div>
+                <div className="setting-item" onClick={() => onLogoutClick()}>
+                  로그아웃
+                </div>
+              </div>
+            )}
+          </div>
+        </>
+      ) : (
+        <AuthBtn />
+      )}
     </Container>
   );
 }
