15b0ec7cab21e0542479ab9c50aab083a5c7375a
HyeonWooGa <yeonwoopark22@gmail.com>
2022-10-29 17:14:49 +0900

    ✨ feat: Loading 컴포넌트 페이지에 적용

--- src/pages/BoardList.js
+++ src/pages/BoardList.js
@@ -1,9 +1,11 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import Ads from "../components/Ads";
 import Aside from "../components/Aside";
 import List from "../components/board/List";
 import BoardMain from "../components/BoardMain";
 import Header from "../components/header/Header";
+import Loading from "../components/Loading";
 
 const Container = styled.div`
   width: 100vw;
@@ -23,15 +25,23 @@ const Main = styled.main`
 `;
 
 function BoardList() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
   return (
-    <Container>
-      <Header />
-      <Main>
-        <Aside />
-        <List path="free" />
-        <Ads />
-      </Main>
-    </Container>
+    <>
+      {loading ? <Loading /> : null}
+      <Container>
+        <Header />
+        <Main>
+          <Aside />
+          <List path="free" />
+          <Ads />
+        </Main>
+      </Container>
+    </>
   );
 }
 
