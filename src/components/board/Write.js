f326549c51338cc80305f70eabb767a1ad57b60a
HyeonWooGa <yeonwoopark22@gmail.com>
2022-11-08 00:58:02 +0900

    ✨ feat: 글 작성 컴포넌트 Balance 탭에 맞게 변경

--- src/components/board/Write.js
+++ src/components/board/Write.js
@@ -1,5 +1,5 @@
 import { addDoc, collection } from "firebase/firestore";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Navigate, useNavigate } from "react-router-dom";
 import styled from "styled-components";
@@ -56,6 +56,7 @@ const WriteForm = styled.form`
     padding-left: 10px;
     border-radius: 10px;
     border: 1px solid ${(props) => props.theme.textColorOpacity};
+    margin-bottom: 20px;
   }
 
   textarea {
@@ -77,40 +78,50 @@ const WriteForm = styled.form`
 `;
 
 const Write = () => {
-  const [topic, setTopic] = useState("사는얘기");
   const [title, setTitle] = useState("");
-  const [text, setText] = useState("");
+  const [textLeft, setTextLeft] = useState("");
+  const [textRight, setTextRight] = useState("");
   const [isSubmitting, SetisSubmitting] = useState(false);
   const currentBoard = useSelector((state) => state.currentBoard.value);
   const currentUser = useSelector((state) => state.currentUser);
-  const { displayName, photoURL } = currentUser;
+  const { displayName } = currentUser;
   const navigate = useNavigate();
 
-  const onTopicChange = (event) => {
-    setTopic(event.target.value);
+  const onTextLeftChange = (event) => {
+    setTextLeft(event.target.value);
   };
 
-  const onTitleChange = (event) => {
-    setTitle(event.target.value);
+  const onTextRightChange = (event) => {
+    setTextRight(event.target.value);
   };
 
-  const onTextChange = (event) => {
-    setText(event.target.value);
-  };
+  console.log(title, textLeft, textRight);
 
   const onClickAccess = async (event) => {
     event.preventDefault();
     if (!isSubmitting) {
       SetisSubmitting(true);
-      if (topic && title && text) {
+      if (textLeft && title && textRight) {
         const createdAt = new Date().toLocaleString();
-        const data = { topic, title, text, createdAt, displayName, photoURL };
-        await addDoc(collection(db, "free"), data);
-        navigate("/free");
+        const data = {
+          title,
+          leftStr: textLeft,
+          leftNum: 0,
+          rightStr: textRight,
+          rightNum: 0,
+          createdAt,
+          emjBad: 0,
+          emjDizzy: 0,
+          emjFunny: 0,
+          emjGood: 0,
+          emjLove: 0,
+          emjSad: 0,
+        };
+        await addDoc(collection(db, "balance"), data);
+        navigate("/");
       }
-      if (!topic) alert("토픽을 선택해주세요");
-      else if (!title) alert("제목을 입력해주세요");
-      else if (!text) alert("내용을 입력해주세요");
+      if (!textLeft) alert("좌항을 입력해주세요");
+      else if (!textRight) alert("우항을 입력해주세요");
       SetisSubmitting(false);
     } else alert("진정하세요...글 올라갑니다.");
   };
@@ -118,6 +129,10 @@ const Write = () => {
   const onCancelClick = () => {
     navigate("/" + currentBoard);
   };
+
+  useEffect(() => {
+    setTitle(`${textLeft} vs ${textRight}`);
+  }, [textLeft, textRight]);
 
   return (
     <>
@@ -132,26 +147,28 @@ const Write = () => {
             </span>
           </Header>
           <WriteForm onSubmit={onClickAccess}>
-            <label htmlFor="topic">토픽</label>
-            <select onChange={onTopicChange} value={topic} id="topic" required>
-              <option>요즘 이야기</option>
-              <option>모임&스터디</option>
-            </select>
+            <label htmlFor="textLeft">좌항</label>
+            <input
+              id="textLeft"
+              placeholder="좌항을 입력해주세요."
+              value={textLeft}
+              onChange={onTextLeftChange}
+              required
+            />
+            <label htmlFor="textRight">우항</label>
+            <input
+              id="textRight"
+              placeholder="우항을 입력해주세요."
+              value={textRight}
+              onChange={onTextRightChange}
+              required
+            />
             <label htmlFor="title">제목</label>
             <input
               id="title"
-              placeholder="제목을 입력해주세요."
-              value={title}
-              onChange={onTitleChange}
-              required
-            />
-            <label htmlFor="text">내용</label>
-            <textarea
-              id="text"
-              placeholder="내용을 입력해주세요."
-              value={text}
-              onChange={onTextChange}
-              required
+              placeholder="좌항과 우항 입력시 자동완성됩니다."
+              value={`${textLeft} vs ${textRight}`}
+              readOnly
             />
             <div>
               <BtnDefault type="button" onClick={onCancelClick}>
