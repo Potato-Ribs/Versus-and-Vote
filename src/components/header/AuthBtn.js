79fcc3b451a4fa52607de4944cea30c785001bb6
HyeonWooGa <yeonwoopark22@gmail.com>
2022-11-08 00:57:23 +0900

    💄 style: 헤더 상단 Auth 버튼 스타일 수정

--- src/components/header/AuthBtn.js
+++ src/components/header/AuthBtn.js
@@ -9,17 +9,20 @@ const Wrapper = styled.div`
 const LoginBtn = styled.button`
   height: 40px;
   width: 80px;
-  border: 1px solid blue;
+  border: none;
   border-radius: 20px;
+  background-color: ${(props) => props.theme.textColor};
+  color: ${(props) => props.theme.bgColor};
 
   :hover {
     cursor: pointer;
+    background-color: ${(props) => props.theme.textColorOpacity};
   }
 `;
 
 const JoinBtn = styled(LoginBtn)`
-  background-color: blue;
-  color: white;
+  color: ${(props) => props.theme.textColor};
+  background-color: ${(props) => props.theme.accentColor};
 `;
 
 function AuthBtn() {
