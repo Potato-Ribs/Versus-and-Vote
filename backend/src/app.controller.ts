6d06abe9f1c306206cd6d99a847271ff8bea39d7
JooYoung2274 <jooooo2274@gmail.com>
2022-11-10 12:13:08 +0900

    ✨feat: typeorm & oauth test setting

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { AppService } from './app.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('oauth 관련 API 입니다. 무시하셔도 됩니다.')
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Get()
+    @Render('index')
+    getHellos(@Res() res: Response) {
+        const google = process.env.OAUTH_GOOGLE_URL as string;
+        const kakao = process.env.OAUTH_KAKAO_URL as string;
+        return {
+            google: google,
+            kakao: kakao,
+        };
+    }
+
+    getHello() {
+        return 'Hello world!';
+    }
 }
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        TypeOrmModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService) => {
+                return {
+                    type: 'mysql',
+                    host: 'localhost',
+                    port: 3306,
+                    username: configService.get('DB_USERNAME'),
+                    password: configService.get('DB_PASSWORD'),
+                    database: configService.get('DB_DATABASE'),
+                    entities: [],
+                    autoLoadEntities: true,
+                    charset: 'utf8mb4',
+                    synchronize: true,
+                    logging: true, // query 날리는것 로깅
+                    // keepConnectionAlive: true, //hot reloading 할때 필요
+                };
+            },
+        }),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
