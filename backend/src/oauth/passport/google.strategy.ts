459e2af334e1b2761cbcdf1b746009887c1b5931
JooYoung2274 <jooooo2274@gmail.com>
2022-11-10 17:57:17 +0900

    ✨feat: google, jwt strategy 추가

---
+++ backend/src/oauth/passport/google.strategy.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, Profile, VerifyCallback } from 'passport-google-oauth20';
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+    constructor() {
+        super({
+            clientID: process.env.OAUTH_GOOGLE_ID,
+            clientSecret: process.env.OAUTH_GOOGLE_SECRET,
+            callbackURL: process.env.OAUTH_GOOGLE_REDIRECT,
+            scope: ['email', 'profile'],
+        });
+    }
+
+    validate(accessToken: string, refreshToken: string, profile: Profile, done: VerifyCallback) {
+        const { name, emails, photos } = profile;
+
+        const user = {
+            provider: 'google',
+            nickname: name,
+            email: emails[0].value,
+            photos: photos[0].value,
+            accessToken,
+            refreshToken,
+        };
+        done(null, user);
+    }
+}
---
+++ backend/src/oauth/passport/jwt.strategy.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+    constructor() {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: process.env.JWT_SECRET,
+        });
+    }
+
+    async validate(payload: any) {
+        console.log(payload);
+        return payload;
+    }
+}
